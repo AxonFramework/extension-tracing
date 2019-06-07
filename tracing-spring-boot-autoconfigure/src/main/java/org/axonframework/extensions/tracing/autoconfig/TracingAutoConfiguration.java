5022e4063c74784a2d11df26e053614a60cd3445
Steven van Beelen <steven.vanbeelen@axoniq.io>
2019-06-07 14:13:03 +0200

    Replace @Primary for @ConditionalOnMissingBean
    There is no evident reason for the @Primary annotation on the gateway
    beans. Additionally, they should be build conditionally, only if no
    gateway bean is present.
    
    Resolves #8

--- tracing-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/tracing/autoconfig/TracingAutoConfiguration.java
+++ tracing-spring-boot-autoconfigure/src/main/java/org/axonframework/extensions/tracing/autoconfig/TracingAutoConfiguration.java
@@ -31,9 +31,9 @@ import org.axonframework.springboot.autoconfig.EventProcessingAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * Auto configure a tracing capabilities.
@@ -57,7 +57,7 @@ public class TracingAutoConfiguration {
     }
 
     @Bean
-    @Primary
+    @ConditionalOnMissingBean
     public QueryGateway queryGateway(QueryBus queryBus,
                                      OpenTraceDispatchInterceptor openTraceDispatchInterceptor,
                                      OpenTraceHandlerInterceptor openTraceHandlerInterceptor,
@@ -71,7 +71,7 @@ public class TracingAutoConfiguration {
     }
 
     @Bean
-    @Primary
+    @ConditionalOnMissingBean
     public CommandGateway commandGateway(CommandBus commandBus,
                                          OpenTraceDispatchInterceptor openTraceDispatchInterceptor,
                                          OpenTraceHandlerInterceptor openTraceHandlerInterceptor,
